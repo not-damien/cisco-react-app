357be851bb276ce3930266c13de6aae56bf07bbe
not-damien <93546644+not-damien@users.noreply.github.com>
2023-03-22 02:04:02 -0700

    made exhibit component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -6,14 +6,26 @@ function App() {
     
     <div className="App">
       <Banner />
+      <Exhibit title={'IP Adress'}/>
     </div>
-    
     
   );
 }
 
 export function Banner(){
-  return <h1>Hello Banner</h1>;
+  return (
+  <><h1>Sextant</h1><hr></hr></>
+  );
+}
+
+export function Exhibit({title}){
+  return (
+  <div class="exhibit">
+  <h3>{title}</h3>
+  <hr></hr>
+  <p>123.345.123</p>
+  </div>
+  );
 }
 
 export default App;
